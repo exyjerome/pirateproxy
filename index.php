a3e76410f57aa25952ba4182c5c67cac118d4626
Unknown <alfi.7@hotmail.com>
2017-11-09 23:51:58 +0000

    wip

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $router->respond('[*:url]', function($request, $response){
 
     $resp   = $client->request('GET', piratebay . $request->param('url'));
     $resp   = $resp->getBody();
-    $resp   = str_replace(piratebay, proxy_url, $resp);
+    $resp   = str_replace('//thepiratebay.org', proxy_url, $resp);
 
     return $resp;
 });
