ca5da8172c51e3621580bd2b0cff30f9f2d958d4
Unknown <alfi.7@hotmail.com>
2017-11-09 23:41:03 +0000

    wip

--- index.php
+++ index.php
@@ -3,15 +3,19 @@
 require (__DIR__ . '/vendor/autoload.php');
 
 define('piratebay', 'http://thepiratebay.org');
+define('proxy_url', 'http://lethargic.alfi.tech:81');
 
 $router = new \Klein\Klein;
 
 $router->respond('[*:url]', function($request, $response){
     $client = new \GuzzleHttp\Client();
     $reqUrl = piratebay . $request->param('url');
-    var_dump($reqUrl);
+
     $resp   = $client->request('GET', piratebay . $request->param('url'));
-    echo $resp->getBody();
+    $resp   = $resp->getBody();
+    $resp   = str_replace(piratebay, proxy_url, $resp);
+
+    return $resp;
 });
 
 $router->dispatch();
