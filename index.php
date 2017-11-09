75e8efc2a809c42912cd2b2c092f5458ddb93e3e
Alfi Potter <alfi.7@hotmail.com>
2017-11-09 23:30:11 +0000

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require (__DIR__ . '/vendor/autoload.php');
+
+define('piratebay', 'http://thepiratebay.org');
+
+$router = new \Klein\Klein;
+
+$router->respond('[*:url]', function($request, $response){
+    $client = new \GuzzleHttp\Client();
+    $reqUrl = piratebay . $request->param('url');
+    var_dump($reqUrl);
+    $resp   = $client->request('GET', piratebay . $request->param('url'));
+    var_dump ($resp->getBody());
+});
+
+$router->dispatch();
